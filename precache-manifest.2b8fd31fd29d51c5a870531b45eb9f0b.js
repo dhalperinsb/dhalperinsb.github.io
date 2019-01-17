76e550914c496fa05a0d0aea433948477963f1ef
doughalperin <doughalperin@hotmail.com>
2019-01-17 07:27:11 -0800

    .

---
+++ precache-manifest.2b8fd31fd29d51c5a870531b45eb9f0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "491d2295841ebdcbbcf0",
+    "url": "/static/js/main.491d2295.chunk.js"
+  },
+  {
+    "revision": "43b9288ec78bd3908f6c",
+    "url": "/static/js/1.43b9288e.chunk.js"
+  },
+  {
+    "revision": "491d2295841ebdcbbcf0",
+    "url": "/static/css/main.6dec20e8.chunk.css"
+  },
+  {
+    "revision": "7dde3e506d6f49c479991b4145ca5aa1",
+    "url": "/index.html"
+  }
+];
